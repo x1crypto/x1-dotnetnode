ba1e186f3b781459798184f267ea3c4ee07b55cb
soma42 <soma42@protonmail.ch>
2020-12-11 14:57:19 +0100

    use Bitcoins DAA for PoW and PoS, i.e. 2016 block average, but moving window

--- src/Networks/Xds/Blockcore.Networks.Xds/Consensus/XdsConsensusOptions.cs
+++ src/Networks/Xds/Blockcore.Networks.Xds/Consensus/XdsConsensusOptions.cs
@@ -17,12 +17,17 @@ namespace Blockcore.Networks.Xds.Consensus
         /// <summary>
         /// The block height (inclusive), where the PosPowRatchet algorithm starts).
         /// </summary>
-       const int PosPowRatchetStartHeightTestNet = 200;
+        const int PosPowRatchetStartHeightTestNet = 200;
 
         /// <summary>
         /// The block height (inclusive), where the PosPowRatchet algorithm starts).
         /// </summary>
         const int PosPowRatchetTargetCalculationHeightTestNet = 575;
+
+        /// <summary>
+        /// The block height (inclusive), where the PosPowRatchet algorithm starts).
+        /// </summary>
+        const int BitcoinRetargetAlgoHeightTestNet = 680;
 
         // TODO: move this to IConsensus
         /// <summary>Time interval in minutes that is used in the retarget calculation.</summary>
@@ -34,7 +39,7 @@ namespace Blockcore.Networks.Xds.Consensus
         {
             this.network = network;
         }
-       
+
 
         /// <inheritdoc />
         public override int GetStakeMinConfirmations(int height, Network network)
@@ -65,12 +70,125 @@ namespace Blockcore.Networks.Xds.Consensus
             return true;
         }
 
-        public override Target GetNextTargetRequired<T>(T chain, ChainedHeader chainedHeader, IConsensus consensus, bool proofOfStake, ILogger logger)
+        public override Target GetNextTargetRequired<T>(T chain, ChainedHeader chainedHeader, IConsensus consensus,
+            bool proofOfStake, ILogger logger)
         {
             var stakeChain = chain as IStakeChain;
 
-            Guard.NotNull(stakeChain, nameof(stakeChain));
+            if (stakeChain == null)
+                throw new ArgumentNullException(nameof(stakeChain));
 
+            if (chainedHeader.Height < BitcoinRetargetAlgoHeightTestNet)
+                return GetNextTargetRequiredRatchetV1(stakeChain, chainedHeader, consensus, proofOfStake, logger);
+
+            return GetNextTargetRequiredBitcoin(stakeChain, chainedHeader, consensus, proofOfStake, logger);
+        }
+
+        public Target GetNextTargetRequiredBitcoin(IStakeChain stakeChain, ChainedHeader chainedHeader, IConsensus consensus, bool proofOfStake, ILogger logger)
+        {
+
+
+            // Genesis block.
+            if (chainedHeader == null)
+            {
+                logger.LogTrace("(-)[GENESIS]:'{0}'", consensus.PowLimit);
+                return consensus.PowLimit;
+            }
+
+            // Target limit for PoS or PoW
+            Target targetLimit = proofOfStake
+                ? new Target(consensus.ProofOfStakeLimitV2)
+                : consensus.PowLimit;
+
+            // Step 1 - Before we really start, take care of the special cases,
+            //          that's not changed from the classic PoS/Pow code.
+
+            // First block, the newer one
+            ChainedHeader lastPowPosChainedBlock = GetLastPowPosChainedBlock(stakeChain, chainedHeader, proofOfStake);
+            if (lastPowPosChainedBlock.Previous == null)
+            {
+                var res = new Target(targetLimit);
+                logger.LogTrace("(-)[FIRST_BLOCK]:'{0}'", res);
+                return res;
+            }
+
+            // Second block, the older one.
+            ChainedHeader prevLastPoWPosBlock = GetLastPowPosChainedBlock(stakeChain, lastPowPosChainedBlock.Previous, proofOfStake);
+            if (prevLastPoWPosBlock.Previous == null)
+            {
+                var res = new Target(targetLimit);
+                logger.LogTrace("(-)[SECOND_BLOCK]:'{0}'", res);
+                return res;
+            }
+
+            // This is used in tests to allow quickly mining blocks.
+            if (!proofOfStake && consensus.PowNoRetargeting)
+            {
+                logger.LogTrace("(-)[NO_POW_RETARGET]:'{0}'", lastPowPosChainedBlock.Header.Bits);
+                return lastPowPosChainedBlock.Header.Bits;
+            }
+
+            if (proofOfStake && consensus.PosNoRetargeting)
+            {
+                logger.LogTrace("(-)[NO_POS_RETARGET]:'{0}'", lastPowPosChainedBlock.Header.Bits);
+                return lastPowPosChainedBlock.Header.Bits;
+            }
+
+
+
+            // Step 2 - Adjust difficulty the Bitcoin way, ported form Bitcoin Code
+
+            // Bitcoin TargetTimespan / Bitcoin TargetSpacing = 1,209,600 / 600 = 2016.
+            // We'll lock at 2016 block intervals of an algorithm, or less, if we have less blocks.
+            int numberOfInterVals = Math.Min(lastPowPosChainedBlock.Height / 2 - 205, 2016); // adjust 2016 down so that we do not run out off blocks iterating back     
+            var targetSpacingSeconds = 256;     // we are targeting an interval of 256 seconds between blocks
+            var targetTimespanTotalSeconds = numberOfInterVals * targetSpacingSeconds; // the total time for 2016 blocks should be 2016 * 256 = 516,096 seconds = 5 days, 23h, 21m 36s
+
+            // The sum of timespans, the last 2016 blocks of an algorithm needed.
+            // Bitcoin can simply subtract the timestamps, but we need to add up one by one,
+            // because we need to subtract each gap time.
+            long sumOfIntervals = 0;
+
+            for (var i = 0; i < numberOfInterVals; i++)
+            {
+                ChainedHeader lastBlock = GetLastPowPosChainedBlock(stakeChain, chainedHeader, proofOfStake);
+                ChainedHeader middleBlock = lastBlock.Previous;
+                ChainedHeader prevLastBlock = GetLastPowPosChainedBlock(stakeChain, lastBlock.Previous, proofOfStake);
+
+                long intervalIncludingGap = lastBlock.Header.Time - prevLastBlock.Header.Time;
+                long gapDuration = middleBlock.Header.Time - prevLastBlock.Header.Time;
+                long actualInterval = intervalIncludingGap - gapDuration;
+                sumOfIntervals += actualInterval;
+
+                // go back two blocks, and repeat
+                chainedHeader = chainedHeader.Previous.Previous;
+            }
+
+            // Limit the amount of adjustment, by cutting off extreme deltas of actual and desired timespan.
+            // sumOfIntervals is now exactly equivalent to what Bitcoin uses here as time delta.
+            TimeSpan actualTimespan = TimeSpan.FromSeconds(sumOfIntervals);
+            TimeSpan targetTimespan = TimeSpan.FromSeconds(targetTimespanTotalSeconds);
+            if (actualTimespan < targetTimespan / 4)
+                actualTimespan = targetTimespan / 4;
+            if (actualTimespan > targetTimespan * 4)
+                actualTimespan = targetTimespan * 4;
+
+            // Re-target. The reference point is the last block of the algorithm.
+            BigInteger newTarget = lastPowPosChainedBlock.Header.Bits.ToBigInteger();
+
+            // new_target = old_target * (actual / desired)
+            newTarget = newTarget.Multiply(BigInteger.ValueOf((long)actualTimespan.TotalSeconds));
+            newTarget = newTarget.Divide(BigInteger.ValueOf(targetTimespanTotalSeconds));
+
+            var finalTarget = new Target(newTarget);
+            if (finalTarget > targetLimit)
+                finalTarget = targetLimit;
+
+            return finalTarget;
+        }
+
+        private Target GetNextTargetRequiredRatchetV1(IStakeChain stakeChain, ChainedHeader chainedHeader, IConsensus consensus, bool proofOfStake, ILogger logger)
+        {
             // Genesis block.
             if (chainedHeader == null)
             {
@@ -122,11 +240,11 @@ namespace Blockcore.Networks.Xds.Consensus
             Debug.Assert(stakeChain.Get(middleBlock.HashBlock).IsProofOfStake() != stakeChain.Get(lastPowPosBlock.HashBlock).IsProofOfStake());
             Debug.Assert(stakeChain.Get(middleBlock.HashBlock).IsProofOfStake() != stakeChain.Get(prevLastPowPosBlock.HashBlock).IsProofOfStake());
             Debug.Assert(middleBlock != null, "can't be null, second block is older than middle block");
-            Debug.Assert(lastPowPosBlock.Height == middleBlock.Height +1, "middle block height must be 1 less than first block");
+            Debug.Assert(lastPowPosBlock.Height == middleBlock.Height + 1, "middle block height must be 1 less than first block");
             Debug.Assert(prevLastPowPosBlock.Height == middleBlock.Height - 1, "middle block height must be 1 more than second block");
 
             // the timestamp of the older block - this would normally be used
-            var prevLastPowPosBlockTime = prevLastPowPosBlock.Header.Time; 
+            var prevLastPowPosBlockTime = prevLastPowPosBlock.Header.Time;
             var middleBlockTime = middleBlock.Header.Time;
 
             // the time in seconds the other algorithm has used, which is must be hidden
